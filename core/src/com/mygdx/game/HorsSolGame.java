f46de3e0ad4e8244bf5d467f190bca55a262539f
Azneka <anissa.mabed@mines-ales.org>
2020-04-16 10:46:06 +0200

    Tests d'écrans

--- core/src/com/mygdx/game/HorsSolGame.java
+++ core/src/com/mygdx/game/HorsSolGame.java
@@ -6,37 +6,45 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.screens.MainGameScreen;
+import com.mygdx.game.screens.MainMenuScreen;
 
 public class HorsSolGame extends Game {
 	SpriteBatch batch;
 	Texture img;
-	Screen screen;
-	int x;
-	int y;
+	BitmapFont font;
+	ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("zeldapixel.png");
-		setScreen(new MainGameScreen(this));
+		font = new BitmapFont();
+		shapeRenderer = new ShapeRenderer();
+		setScreen(new MainMenuScreen(this));
 	}
 
-	@Override
-	public void render () {
-		super.render();
-	}
+	//@Override
+	//public void render () {
+	//	super.render();
+	//}
 
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		font.dispose();
+		shapeRenderer.dispose();
 	}
 
 	public SpriteBatch getBatch() {
 		return batch;
 	}
+
+	public BitmapFont getFont() { return font; }
 
 }
